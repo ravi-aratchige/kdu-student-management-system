279e2fee608f80688dccc199742df359f386e361
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-03-29 16:04:13 +0530

    FEAT: navigation bar component

---
+++ client/app/components/navigationBar.js
@@ -0,0 +1,26 @@
+import { Heading } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
+
+export default function NavigationBar() {
+    return (
+        <Flex
+            height={'6.5rem'}
+            backgroundColor={'teal.500'}
+            color={'white'}
+            justify={'space-between'}
+            position={'fixed'}
+            top={'0'}
+            left={'0'}
+            right={'0'}
+            zIndex={'1'}
+        >
+            <Center paddingLeft={'2rem'}>
+                <Heading>KDU Student Management System</Heading>
+            </Center>
+            <Center paddingRight={'2rem'}>
+                <h1>Settings</h1>
+            </Center>
+        </Flex>
+    );
+}
