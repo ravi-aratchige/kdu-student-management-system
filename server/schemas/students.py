86c9d2359791ccb05d892e00a435b267a8aa46e0
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-03-30 19:19:10 +0530

    FEAT: students model, schema and controller updated

--- server/schemas/students.py
+++ server/schemas/students.py
@@ -8,6 +8,8 @@ class StudentSchema(BaseModel):
     reg_number: str
     first_name: str
     last_name: str
+    gender: str = "Male"
+    phone_number: int = 0
     address: str
     email: EmailStr
     date_of_birth: date
@@ -16,63 +18,4 @@ class StudentSchema(BaseModel):
     active_course_codes: List[str]
     completed_course_codes: List[str]
     semester: int
-    batch: int
-
-
-# Detailed schema for students (not recommended for use)
-class DetailedStudentSchema(BaseModel):
-    id: int
-    reg_number: str = Field(
-        ...,
-        description="Registration number of the student",
-        example="BSE/39/0001",
-    )
-    first_name: str = Field(
-        ...,
-        description="First name of the student",
-    )
-    last_name: str = Field(
-        ...,
-        description="Last name of the student",
-    )
-    address: str = Field(
-        ...,
-        description="Residential address of the student",
-    )
-    email: EmailStr = Field(
-        ...,
-        description="Email address of the student",
-        example="39-bse-0001@kdu.ac.lk",
-    )
-    date_of_birth: date = Field(
-        ...,
-        description="Date of birth of the student",
-        example="2000-01-01",
-    )
-    nic_number: str = Field(
-        ...,
-        description="NIC number of the student",
-        example="200012301234",
-    )
-    degree: str = Field(
-        ...,
-        description="Degree program of the student",
-    )
-    active_courses: List[str] = Field(
-        [],
-        description="List of courses being followed by the student",
-    )
-    completed_courses: List[str] = Field(
-        [],
-        description="List of courses completed by the student",
-    )
-    semester: int = Field(
-        ...,
-        description="Current semester of the student",
-        example=2,
-    )
-    batch: int = Field(
-        ...,
-        description="Batch (intake) of the student",
-        example=39,
-    )
+    intake: int
