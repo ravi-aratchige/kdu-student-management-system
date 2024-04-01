ed9569e1af7f715d747c7314d4d061de0fb187a9
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-04-02 00:08:37 +0530

    UPDATE: modal for system info + more changes

--- client/app/settings/page.js
+++ client/app/settings/page.js
@@ -1,25 +1,48 @@
+'use client';
+
 import { Box } from '@chakra-ui/react';
 import { Flex } from '@chakra-ui/react';
-import { Link } from '@chakra-ui/react';
 import { Text } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 import { Heading } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+import { useDisclosure } from '@chakra-ui/react';
 import NavigationBar from '../components/navigationBar';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react';
 
 export default function Page() {
+    // Setup router to handle redirects
+    const router = useRouter();
+
+    // State for modal management
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     return (
         <Box paddingTop={'6.5rem'}>
             <NavigationBar />
             {/* Page layout */}
             <Box paddingX={'2rem'} paddingTop={'3rem'}>
+                {/* Page heading */}
                 <Heading size={'3xl'} paddingBottom={'3rem'}>
                     Settings
                 </Heading>
+
+                {/* Page buttons */}
                 <Flex w={'60%'}>
-                    {/* Update Admin Credentials */}
-                    <Link
-                        href={'/settings/database'}
+                    {/* 1. "Update Admin Credentials" */}
+                    <Box
+                        _hover={{ cursor: 'pointer', color: 'teal.500' }}
                         marginRight={'2rem'}
                         w={'33%'}
+                        onClick={() => router.push('/settings/database')}
                     >
                         <Text
                             padding={'2rem'}
@@ -29,13 +52,14 @@ export default function Page() {
                         >
                             Update Database Credentials
                         </Text>
-                    </Link>
+                    </Box>
 
-                    {/* Check Logs */}
-                    <Link
-                        href={'/settings/logs'}
+                    {/* 2. "Check Logs" */}
+                    <Box
+                        _hover={{ cursor: 'pointer', color: 'teal.500' }}
                         marginRight={'2rem'}
                         w={'33%'}
+                        onClick={() => router.push('/settings/logs')}
                     >
                         <Text
                             padding={'2rem'}
@@ -45,13 +69,14 @@ export default function Page() {
                         >
                             Check Logs
                         </Text>
-                    </Link>
+                    </Box>
 
-                    {/* About KDU SMS */}
-                    <Link
-                        href={'/settings/info'}
+                    {/* 3. "About KDU SMS" */}
+                    <Box
+                        _hover={{ cursor: 'pointer', color: 'teal.500' }}
                         marginRight={'2rem'}
                         w={'33%'}
+                        onClick={onOpen}
                     >
                         <Text
                             padding={'2rem'}
@@ -61,8 +86,33 @@ export default function Page() {
                         >
                             About KDU SMS
                         </Text>
-                    </Link>
+                    </Box>
                 </Flex>
+
+                {/* Modal (for system info) */}
+                <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>KDU Student Management System</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <Text>
+                                You are using version 0.1.0 of the KDU Student
+                                Management System.
+                            </Text>
+                        </ModalBody>
+
+                        <ModalFooter>
+                            <Button
+                                colorScheme={'teal'}
+                                mr={3}
+                                onClick={onClose}
+                            >
+                                OK
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
             </Box>
         </Box>
     );
